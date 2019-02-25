eca165ffb2a3391cbb3b143d875962dab1f2efde
Paul Nicholas <liquidream@hotmail.co.uk>
2019-02-25 06:55:30 +0000

    Added RGB conversion helper

--- main.lua
+++ main.lua
@@ -5,15 +5,18 @@
 -- https://love2d-community.github.io/love-api/
 
 local total_time_elapsed = 0
-local playerPos = {
+local player = {
  x = 100,
- y = 100
+ y = 100,
+ size = 20
 }
+local myColour = {44, 232, 245}
 
 function love.draw()
- love.graphics.setColor(1.0, 0.4, 0.4, 1.0)
+ love.graphics.setColor(fromRGBtable(myColour))
+ --love.graphics.setColor(fromRGB(44, 232, 245))
  --love.graphics.setColor(0.4, 0.4, 1.0, 1.0)
- love.graphics.circle("fill", playerPos.x, playerPos.y, 20, 32)
+ love.graphics.circle("fill", player.x, player.y, player.size, 32)
  
  --local y_offset = 8 * math.sin(total_time_elapsed * 3)
   -- love.graphics.print('Edit main.lua to get started!', 400, 300 + y_offset)
@@ -24,23 +27,33 @@ function love.update(dt)
   total_time_elapsed = total_time_elapsed + dt
   
   local speed = 256
-  local y_offset = 8 * math.sin(total_time_elapsed * 3)
+  local y_offset = .5 * math.sin(total_time_elapsed * 3)
   
   if love.keyboard.isDown("right") then
-   playerPos.x = playerPos.x + speed * dt
+   player.x = player.x + speed * dt
   end
   if love.keyboard.isDown("left") then
-   playerPos.x = playerPos.x - speed * dt
+   player.x = player.x - speed * dt
   end
   if love.keyboard.isDown("up") then
-   playerPos.y = playerPos.y - speed * dt
+   player.y = player.y - speed * dt
    --print("up arrow pressed at "..total_time_elapsed)
   end
   if love.keyboard.isDown("down") then
-   playerPos.y = playerPos.y + speed * dt
+   player.y = player.y + speed * dt
   end
 
   -- playing around
-  --playerPos.y = playerPos.y + y_offset
+  player.size = player.size + y_offset
 end
 
+
+-- convert a traditional RBG color table to normalised RGB table
+function fromRGBtable(rgb_table)
+ return {rgb_table[1]/255, rgb_table[2]/255, rgb_table[3]/255}
+end
+
+-- convert a traditional RBG color to normalised RGB
+function fromRGB(red, green, blue) -- alpha?
+ return {red/255, green/255, blue/255}
+end
